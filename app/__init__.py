4328ede0ea7a744e7608f965b930078b5e7a8517
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-06-22 18:20:49 -0400

    Added icon that links to Jiya's portfolio

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 import os
+# import Flask module
 from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 load_dotenv()
+# create Flask server (__name__ means the current file)
 app = Flask(__name__)
 
 
+# default page
 @app.route('/')
 def index():
+    # render_template(): searches for specified template and renders it
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+# route to /jiya_portfolio
+@app.route('/jiya_portfolio')
+def jiya_portfolio():
+    return render_template('jiya_portfolio.html')
