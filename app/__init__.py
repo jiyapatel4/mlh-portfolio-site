1e12f037d947cf34f643f28e5db96f3ffc77f267
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-06-24 12:46:16 -0400

    Added 404 error handling and modified render_template()

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,16 @@
 import os
 # import Flask module
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from dotenv import load_dotenv
 
 load_dotenv()
 # create Flask server (__name__ means the current file)
 app = Flask(__name__)
+
+# decorator registers function as a custom error handler
+@app.errorhandler(404)
+def page_not_found(error):
+    render_template('404.html'), 404
 
 
 # default page
@@ -17,4 +22,4 @@ def index():
 # route to /jiya_portfolio
 @app.route('/jiya_portfolio')
 def jiya_portfolio():
-    return render_template('jiya_portfolio.html')
+    return render_template('jiya_portfolio.html', title="About Jiya", url=os.getenv("URL"))
