4c1bedc078e51f1c518deeb73d7ad82e6ce65a5e
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-06-28 12:27:37 -0400

    Fixed icons on homepage

--- app/__init__.py
+++ app/__init__.py
@@ -11,40 +11,52 @@ load_dotenv()
 app = Flask(__name__)
 
 # decorator registers function as a custom error handler
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     render_template('404.html'), 404
 
 # default page
+
+
 @app.route('/')
 def index():
     # render_template(): searches for specified template and renders it
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MLH Fellows", url=os.getenv("URL"))
 
 # routes within Jiya's portfolio
+
+
 @app.route('/jiya_base')
 def jiya_base():
     return render_template('jiya/jiya_base.html', title="About Jiya", url=os.getenv("URL"))
+
 
 @app.route('/jiya_work')
 def jiya_work():
     return render_template('jiya/jiya_work.html', title="Jiya's Work", url=os.getenv("URL"))
 
+
 @app.route('/jiya_hobbies')
 def jiya_hobbies():
     return render_template('jiya/jiya_hobbies.html', title="Jiya's Hobbies", url=os.getenv("URL"))
+
 
 @app.route('/chizy_base')
 def chizy_base():
     return render_template('chizy/chizy_base.html', title="About Chizy", url=os.getenv("URL"))
 
+
 @app.route('/chizy_work')
 def chizy_work():
     return render_template('chizy/chizy_work.html', title="Chizy's Work", url=os.getenv("URL"))
 
+
 @app.route('/chizy_hobbies')
 def chizy_hobbies():
     return render_template('chizy/chizy_hobbies.html', title="Chizy's Hobbies", url=os.getenv("URL"))
+
 
 # put app in debug mode
 if __name__ == "__main__":
