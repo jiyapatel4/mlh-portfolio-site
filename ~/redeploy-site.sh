a8a90731047ba9b7f1069a7b7e30ec8eb267838a
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-07-09 02:16:20 -0400

    Update redeploy-site.sh

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -5,5 +5,4 @@ git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
-tmux new-session -d
-flask run --host=jiya-patel.duckdns.org
+tmux new-session -d 'flask run --host=jiya-patel.duckdns.org'
