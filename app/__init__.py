d8da3af3c332c217ad30b405425b5b233f299d43
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2023-07-21 15:23:43 -0400

    Made changes to mydb

--- app/__init__.py
+++ app/__init__.py
@@ -15,9 +15,9 @@ app = Flask(__name__)
 # use MySQLDatabase class to connect with a MySQL database on network
 mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
               user=os.getenv("MYSQL_USER"),
-              password=os.getenv("MYSQL_PASSWORD"),
-              host=os.getenv("MYSQL_HOST"),
-              port=3306         
+              password=os.getenv("MYSQL_PASSWORD")
+            #   host=os.getenv("MYSQL_HOST"),
+            #   port=3306         
         )
 
 print(mydb)
