4e414b70f53e85ff1797fa13b810fb5a2130908b
Jiya Patel <128255337+jiyapatel4@users.noreply.github.com>
2023-07-22 22:35:37 -0400

    Update redeploy-site.sh

--- ~/redeploy-site.sh
+++ ~/redeploy-site.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-tmux kill-server
 cd mlh-portfolio-site-deploy
 git fetch && git reset origin/main --hard
 python -m venv python3-virtualenv
 source python3-virtualenv/bin/activate
 pip install -r requirements.txt
 
-tmux new-session -d -s "$test"
-tmux send-keys -t "$test" 'python -m venv python3-virtualenv' Enter 'source python3-virtualenv/bin/activate' Enter 'flask run --host=jiya-patel.duckdns.org' Enter
+systemctl daemon-reload
+systemctl restart myportfolio
